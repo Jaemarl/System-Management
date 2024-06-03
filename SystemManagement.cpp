625cb59b68aa9576cf76ce3cf721a3e547ef594a
Jaemarl Mossesgeld <112617008+Jaemarl@users.noreply.github.com>
2024-06-03 16:56:25 +0800

    Update SystemManagement.cpp

--- SystemManagement.cpp
+++ SystemManagement.cpp
@@ -15,31 +15,38 @@ int main()
     string studentUsernames[50 * 5];
     string studentPasswords[50 * 5];
 
-    int staffCount = 0;
+    int staffCount, i = 0;
     int teacherCount = 0;
     int studentCount = 0;
 
-    while (true) {
-        cout << "Student Management System\n" << endl;
+    while (true)
+    {
+        cout << "Student Management System\n"
+             << endl;
         cout << "1. Login\n";
         cout << "2. Exit\n";
         cout << "Enter your choice: ";
         int mainChoice;
         cin >> mainChoice;
 
-        if (mainChoice == 2) {
+        if (mainChoice == 2)
+        {
             cout << "Exiting the program.\n";
             break;
-        } else if (mainChoice == 1) {
+        }
+        else if (mainChoice == 1)
+        {
             cout << "Username: ";
             cin >> username;
             cout << "Password: ";
             cin >> password;
 
             // Admin Login
-            if (username == adminUsername && password == adminPassword) {
+            if (username == adminUsername && password == adminPassword)
+            {
                 int adminChoice;
-                do {
+                do
+                {
                     cout << "\nAdmin Menu:\n";
                     cout << "1. Create Staff Account\n";
                     cout << "2. Create Teacher Account\n";
@@ -47,77 +54,101 @@ int main()
                     cout << "Enter your choice: ";
                     cin >> adminChoice;
 
-                    if (adminChoice == 1 && staffCount < 3) {
+                    if (adminChoice == 1 && staffCount < 3)
+                    {
                         cout << "Enter staff username: ";
                         cin >> staffUsernames[staffCount];
                         cout << "Enter staff password: ";
                         cin >> staffPasswords[staffCount];
                         staffCount++;
                         cout << "Staff account created successfully.\n";
-                    } else if (adminChoice == 1 && staffCount >= 3) {
+                    }
+                    else if (adminChoice == 1 && staffCount >= 3)
+                    {
                         cout << "Maximum staff accounts reached.\n";
-                    } else if (adminChoice == 2 && teacherCount < 5) {
+                    }
+                    else if (adminChoice == 2 && teacherCount < 5)
+                    {
                         cout << "Enter teacher username: ";
                         cin >> teacherUsernames[teacherCount];
                         cout << "Enter teacher password: ";
                         cin >> teacherPasswords[teacherCount];
                         teacherCount++;
                         cout << "Teacher account created successfully.\n";
-                    } else if (adminChoice == 2 && teacherCount >= 5) {
+                    }
+                    else if (adminChoice == 2 && teacherCount >= 5)
+                    {
                         cout << "Maximum teacher accounts reached.\n";
-                    } else if (adminChoice != 3) {
+                    }
+                    else if (adminChoice != 3)
+                    {
                         cout << "Invalid choice, please try again.\n";
                     }
                 } while (adminChoice != 3);
             }
             // Staff Login
-            else {
+            else
+            {
                 bool isStaff = false;
-                for (int i = 0; i < staffCount; i++) {
-                    if (username == staffUsernames[i] && password == staffPasswords[i]) {
+                for (i = 0; i < staffCount; i++)
+                {
+                    if (username == staffUsernames[i] && password == staffPasswords[i])
+                    {
                         isStaff = true;
                         cout << "Staff logged in successfully.\n";
                         // Add staff-specific actions here if needed
                         break;
                     }
                 }
-                if (isStaff) continue;
+                if (isStaff)
+                    continue;
 
                 // Teacher Login
                 bool isTeacher = false;
-                for (int i = 0; i < teacherCount; i++) {
-                    if (username == teacherUsernames[i] && password == teacherPasswords[i]) {
+                for (i = 0; i < teacherCount; i++)
+                {
+                    if (username == teacherUsernames[i] && password == teacherPasswords[i])
+                    {
                         isTeacher = true;
                         int teacherChoice;
-                        do {
+                        do
+                        {
                             cout << "\nTeacher Menu:\n";
                             cout << "1. Create Student Account\n";
                             cout << "2. Logout\n";
                             cout << "Enter your choice: ";
                             cin >> teacherChoice;
 
-                            if (teacherChoice == 1 && studentCount < 50 * 5) {
+                            if (teacherChoice == 1 && studentCount < 50 * 5)
+                            {
                                 cout << "Enter student username: ";
                                 cin >> studentUsernames[studentCount];
                                 cout << "Enter student password: ";
                                 cin >> studentPasswords[studentCount];
                                 studentCount++;
                                 cout << "Student account created successfully.\n";
-                            } else if (teacherChoice == 1 && studentCount >= 50 * 5) {
+                            }
+                            else if (teacherChoice == 1 && studentCount >= 50 * 5)
+                            {
                                 cout << "Maximum student accounts reached.\n";
-                            } else if (teacherChoice != 2) {
+                            }
+                            else if (teacherChoice != 2)
+                            {
                                 cout << "Invalid choice, please try again.\n";
                             }
                         } while (teacherChoice != 2);
                         break;
                     }
                 }
-                if (isTeacher) continue;
+                if (isTeacher)
+                    continue;
 
                 // Student Login
                 bool isStudent = false;
-                for (int i = 0; i < studentCount; i++) {
-                    if (username == studentUsernames[i] && password == studentPasswords[i]) {
+                for (i = 0; i < studentCount; i++)
+                {
+                    if (username == studentUsernames[i] && password == studentPasswords[i])
+                    {
                         isStudent = true;
                         cout << "Student logged in successfully.\n";
                         // Add student-specific actions here if needed
@@ -125,11 +156,14 @@ int main()
                     }
                 }
 
-                if (!isStudent) {
+                if (!isStudent)
+                {
                     cout << "Invalid credentials, please try again.\n";
                 }
             }
-        } else {
+        }
+        else
+        {
             cout << "Invalid choice, please try again.\n";
         }
     }
